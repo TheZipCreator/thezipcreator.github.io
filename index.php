f92b41e9c2f770508a4128ed5ad687306638d5e1
TheZipCreator <wooterxx09@gmail.com>
2019-06-07 17:04:02 -0400

    Added a shit ton more things

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  require 'header.php';
+?>
+<div class='center'>
+  Hello welcome to my site. Here I have many different explorable explanations, mostly about politics and sometimes other topics I care about. There is a list of links below for you to look at:<br>
+  
+</div>
